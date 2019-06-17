d30fbfbc21ea4375333481c938e07269a69f1e5d
jc-white <jarinwhite@gmail.com>
2019-06-16 23:41:15 -0500

    Update encodePassword.ts
    Removed obsolete encodePassword function

--- src/util/encodePassword.ts
+++ src/util/encodePassword.ts
@@ -1,14 +1,3 @@
-/*export function encodePassword(password: string) {
-	let newPassword = '';
-	password = "\"" + password + "\"";
-
-	for (let i = 0; i < password.length; i++) {
-	    newPassword += String.fromCharCode( password.charCodeAt(i) & 0xFF,(password.charCodeAt(i) >>> 8) & 0xFF);
-	}
-
-	return newPassword;
-}*/
-
 export function encodePassword(password: string) {
     return new Buffer('"' + password + '"', 'utf16le').toString();
 }
